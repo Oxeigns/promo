5b92c2f01933d82d8360c4f1100583b046d18bf2
Samrat <yashsamrat10080@gmail.com>
2025-07-10 13:06:38 +0530

    Fix missing export for increment_join_count

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,14 @@
 from .mongo import db
-from .users import users_col, ensure_user, get_user, increment_credits, update_premium, ban_user, get_all_users
+from .users import (
+    users_col,
+    ensure_user,
+    get_user,
+    increment_credits,
+    update_premium,
+    ban_user,
+    get_all_users,
+    increment_join_count,
+)
 from .groups import (
     groups_col,
     get_active_group,
@@ -13,7 +22,7 @@ from .payments import payments_col, insert_payment, get_latest_payment, update_p
 from .referrals import add_referral, get_referral, increment_referral_join
 
 __all__ = [
-    "db", "users_col", "ensure_user", "get_user", "increment_credits", "update_premium", "ban_user", "get_all_users",
+    "db", "users_col", "ensure_user", "get_user", "increment_credits", "update_premium", "ban_user", "get_all_users", "increment_join_count",
     "groups_col", "get_active_group", "increment_promo", "submit_group", "get_group_by_owner",
     "approve_group", "ban_group",
     "payments_col", "insert_payment", "get_latest_payment", "update_payment_status",
